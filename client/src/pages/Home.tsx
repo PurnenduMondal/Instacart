d27be13ff6ea57a80df2e62d18b8ed7f63e6c5c2
PurnenduMondal <purnendum@rapidquest.in>
2026-05-21 16:36:01 +0530

    Home Page Complate

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,19 @@
+import AppPromoBanner from '../components/Home/AppPromoBanner'
 import Features from '../components/Home/Features'
 import Hero from '../components/Home/Hero'
+import HomeCategories from '../components/Home/HomeCategories'
+import NewsLatter from '../components/Home/NewsLatter'
+import PopularProducts from '../components/Home/PopularProducts'
 
 const Home = () => {
   return (
     <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <Hero />
       <Features />
+      <HomeCategories />
+      <PopularProducts />
+      <AppPromoBanner />
+      <NewsLatter />
     </div>
     
   )
